92041eefaf9c7a239e7027df8abc583f9f9ce474
devonhg <devon.hugh.godfrey@gmail.com>
2015-06-08 14:29:09 -0400

    1.1.5 Modified news output to show only 10 posts

--- games-plugin.php
+++ games-plugin.php
@@ -4,7 +4,7 @@ if ( ! defined( 'WPINC' ) ) { die; }
  * Plugin Name:       Games Plugin
  * Plugin URI:        dhgodfrey.net
  * Description:       This plugin is designed to track games developed.
- * Version:           1.1.4
+ * Version:           1.1.5
  * Author:            Devon Godfrey
  * Author URI:        http://playfreygames.net
  * License:           GPL-2.0+
@@ -70,7 +70,7 @@ function GMEPLG_latest_news( $quer = null ){
 
     $args = array(
         "category_name" => $post->post_title, 
-        "posts_per_page" => -1,
+        "posts_per_page" => 10,
     );
 
     $query = new WP_Query( $args );
